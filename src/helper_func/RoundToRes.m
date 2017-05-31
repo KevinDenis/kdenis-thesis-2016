7c9f33e01d81d1df0a7d1e5b08d16cc4c57da23f
Kevin Denis <kevin.denis@student.kuleuven.be>
2017-05-31 11:57:55 +0200

    Removed unique operation for speed performances

--- src/helper_func/RoundToRes.m
+++ src/helper_func/RoundToRes.m
@@ -2,7 +2,7 @@ function [ XY_Round ] = RoundToRes(XY,res)
 %[ XY_Round ] = RoundToRes(XY,res)
 XY_Round=round(XY./res).*res;
 XY_Round=round(XY_Round*100)./100;
-XY_Round=unique(XY_Round,'rows'); 
+% XY_Round=unique(XY_Round,'rows'); 
 % a second rounding operation is needed due to small numerical errors
 % during the multiplication of the previous step.
 % Alltough unique is a costly operation, this will speed up the dynamical
