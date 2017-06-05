861933fb8e1b27d0384ccdcf0590050382d3a161
Kevin Denis <kevin.denis@student.kuleuven.be>
2017-06-05 14:51:50 +0200

    added comma separation for robustness

--- src/helper_func/getStartEndVerticesPath.m
+++ src/helper_func/getStartEndVerticesPath.m
@@ -1,5 +1,5 @@
 function [vertices] = getStartEndVerticesPath(Path)
 %[vertices] = getStartEndVerticesPath(Path)
-vertices=[[Path.x0].',[Path.y0].',[Path.th0].' [Path.x1].',[Path.y1].',[Path.th1].'];
+vertices=[[Path.x0].', [Path.y0].', [Path.th0].', [Path.x1].', [Path.y1].', [Path.th1].'];
 end
 
