cea91dd9ebd72f0c79f47991ce88f6ebb5ce8b03
Kevin Denis <kevin.denis@student.kuleuven.be>
2017-06-06 12:41:06 +0200

    Updated with new helper_func to get vertices from LSL

--- src/helper_func/getMotionPrimitiveFromStateLattice.m
+++ src/helper_func/getMotionPrimitiveFromStateLattice.m
@@ -1,6 +1,6 @@
 function [MP] = getMotionPrimitiveFromStateLattice(LSL)
 %[MP] = getMotionPrimitiveFromStateLattice(LSL)
-[vertices] = getForwardMotionFromStateLattice(LSL);
+[vertices] = getStartEndVerticesPath(LSL);
 idxOrigin=ismember(vertices(:,1:2), [0 0],'rows');
 MP=LSL(idxOrigin,:);
 end
