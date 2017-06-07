f1e79dbb599931d82c0aae7280b0a0161fc76587
Kevin Denis <kevin.denis@student.kuleuven.be>
2017-06-07 10:53:34 +0200

    Removed old parts

--- src/helper_plot/plotPath.m
+++ src/helper_plot/plotPath.m
@@ -1,8 +1,9 @@
 function plotPath(Path,varargin)
-%PLOTPATH Summary of this function goes here
-%   Detailed explanation goes here
+%plotPath(Path,varargin)
 %   Notes :
-%   * quickly plots in one shot thanks to nan (could also be inf)
+%   * quickly plots in one shot thanks to nan (could also be inf). This
+%   improves considerably the time needed to plot large sets of paths
+
 co=get(groot,'DefaultAxesColorOrder');
 
 if nargin == 1
@@ -23,44 +24,13 @@ elseif nargin == 4
     plotStyle=(varargin{3});
 end
 
-PathFree=Path([Path.free]);
-
-for ii=1:length(Path)
-    if ~Path(ii).free
-        idxPlot=Path(ii).idxBlocked;
-        Path(ii).X=Path(ii).X(1:idxPlot-1);
-        Path(ii).Y=Path(ii).Y(1:idxPlot-1);
-        Path(ii).TH=Path(ii).TH(1:idxPlot-1);
-    end
+% Adjust path length if blocked somewhere
+for ii=find(~[Path.free])
+    idxPlot=Path(ii).idxBlocked;
+    Path(ii).X=Path(ii).X(1:idxPlot-1);
+    Path(ii).Y=Path(ii).Y(1:idxPlot-1);
+    Path(ii).TH=Path(ii).TH(1:idxPlot-1);
 end
-
-
-% [Path_Circ,Path_Cloth] = sortPathPlot(Path);
-% X_Circ=zeros(1);
-% Y_Circ=zeros(1);
-% kk=0;
-% for ii=1:length(Path_Circ)
-%     kk=kk(end)+(1:(length(Path_Circ(ii).X)+1));
-%     X_Circ(kk)=[Path_Circ(ii).X;nan];
-%     Y_Circ(kk)=[Path_Circ(ii).Y;nan]; 
-% end
-% X_Cloth=zeros(1);
-% Y_Cloth=zeros(1);
-% kk=0;
-% for ii=1:length(Path_Cloth)
-%     kk=kk(end)+(1:(length(Path_Cloth(ii).X)+1));
-%     X_Cloth(kk)=[Path_Cloth(ii).X;nan];
-%     Y_Cloth(kk)=[Path_Cloth(ii).Y;nan]; 
-% end
-
-% X_Cloth=zeros(1);
-% Y_Cloth=zeros(1);
-% kk=0;
-% for ii=1:length(Path_Cloth)
-%     kk=kk(end)+(1:(length(Path_Cloth(ii).X)+1));
-%     X_Cloth(kk)=[Path_Cloth(ii).X;nan];
-%     Y_Cloth(kk)=[Path_Cloth(ii).Y;nan]; 
-% end
 
 X_curve=zeros(1);
 Y_curve=zeros(1);
@@ -71,12 +41,10 @@ for ii=1:length(Path)
     Y_curve(kk)=[Path(ii).Y;nan]; 
 end
 
-XY=unique([[PathFree.x0].',[PathFree.y0].';[PathFree.x1].',[PathFree.y1].'],'rows');
     
 plot(X_curve,Y_curve,plotStyle,'Color',plotColor,'Linewidth',lineWidthPlot);  
-% plot(X_Circ,Y_Circ,'Color',co(2,:),'Linewidth',2);
 
-% if ~isempty(XY) && size(XY,1)>5; plot(XY(:,1),XY(:,2),'k*','MarkerSize',15); end
+
 
 
 %% I have to have MotPrem settings to plot this !
