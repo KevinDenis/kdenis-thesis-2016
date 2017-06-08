d4f72dfe9fb3d058efb9b626fccd56ec7dd28fe6
Kevin Denis <kevin.denis@student.kuleuven.be>
2017-06-08 09:48:51 +0200

    Updated due to new helper_func

--- src/helper_func/CleanupLooseStarts.m
+++ src/helper_func/CleanupLooseStarts.m
@@ -5,11 +5,11 @@ function [Path] = CleanupLooseStarts(Path)
 %   * This relies on the fact that a cloth connection is max 2
 vertices = getStartEndVerticesPath(Path);
 for ii=1:size(vertices,1)
-    voxel_ii=vertices(ii,:);
-    idxOriginToStart=findVector(vertices,[0 0 0 voxel_ii(1:3)]);
-    if ~(voxel_ii(1)==0 && voxel_ii(2)==0 && voxel_ii(3)==0)  && ~Path(idxOriginToStart).free  
+    vertex_ii=vertices(ii,:);
+    idxOriginToStart=findrow_mex(vertices,[0 0 0 vertex_ii(1:3)]);
+    if ~(vertex_ii(1)==0 && vertex_ii(2)==0 && vertex_ii(3)==0)  && ~Path(idxOriginToStart).free  
         Path(ii).free=false;
-        Path(ii).idxBlocked=1;
+        Path(ii).idxBlocked=1; % it is blocked at the start, since predecessor is also blocked
     end
 end
 end
