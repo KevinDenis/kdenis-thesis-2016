dcb25d35cd5ffef07f33fbbce8c207ff7faa1acd
Kevin Denis <kevin.denis@student.kuleuven.be>
2017-06-28 10:58:22 +0200

    Added comment

--- src/helper_plot/plotGrid.m
+++ src/helper_plot/plotGrid.m
@@ -1,5 +1,5 @@
 function plotGrid(grid_XY,robotPose)
-%plotSimpleRobot(robotPos)
+%plotGrid(grid_XY,robotPose)
 %   Detailed explanation goes here
 grid_XY_W=RotTransXY(grid_XY,robotPose(3),robotPose(1),robotPose(2));
 plot(grid_XY_W(:,1),grid_XY_W(:,2),'o','Color',[0.5 .5 .5],'MarkerSize',10)
