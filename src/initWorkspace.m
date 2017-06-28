ea84f766d234d5b4aa22a81d510ed8df110fb5c7
Kevin Denis <kevin.denis@student.kuleuven.be>
2017-06-28 10:57:15 +0200

    Added additional paths to workspace

--- src/initWorkspace.m
+++ src/initWorkspace.m
@@ -1,7 +1,11 @@
-clearvars -except grid_XY XY_ObsTable LSLset LSL LSL_W ROI0 ROI1 idxIn0 idxIn1 robotPose LabGrid ObstacleTable XY_occ_lab_R path StateLattice
+clearvars -except grid_XY XY_ObsTable LSLset LSL LSL_W ROI0 ROI1 idxIn0  ... 
+            idxIn1 robotPose LabGrid ObstacleTable XY_occ_lab_R path StateLattice ...
+            ObstacleTable_cloth XY_ObsTable_cloth LSL_cloth LSL_W_cloth ...
+            ObstacleTable_circ XY_ObsTable_circ LSL_circ LSL_W_circ
 close all
 clc
 addpath('helper_external');
+addpath('helper_external/ParforProgress2');
 addpath('helper_external/export_fig');
 addpath('helper_external/ClothoidG1fitting');
 addpath('helper_func/BezierCurve');
