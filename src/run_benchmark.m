0c3a2e1a519be5c1cea02cf98f8e3a517c635a8e
Kevin Denis <kevin.denis@student.kuleuven.be>
2017-07-04 09:50:38 +0200

    Updated settings benchmark main file

--- src/run_benchmark.m
+++ src/run_benchmark.m
@@ -9,6 +9,6 @@ Map_ResTestGrid=sortrows(Map_ResTestGrid,2); % sort with increasing computing ti
 for ii=1:size(Map_ResTestGrid,1)
     Map_ii = Map_ResTestGrid(ii,1);
     ResTestGrid_ii = Map_ResTestGrid(ii,2);
-    LPT_Benchmark_calc(Map_ii,ResTestGrid_ii)
-% 	LPT_Benchmark_plot(Map_ii,ResTestGrid_ii,0)
+%     LPT_Benchmark_calc(Map_ii,ResTestGrid_ii)
+	LPT_Benchmark_plot(Map_ii,ResTestGrid_ii,0)
 end
