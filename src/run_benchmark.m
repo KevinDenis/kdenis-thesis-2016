88cc8753abc0700f19a73d335dd5dd9cacb547b2
Kevin Denis <kevin.denis@student.kuleuven.be>
2017-08-11 20:15:59 +0200

    Changed user parameters

--- src/run_benchmark.m
+++ src/run_benchmark.m
@@ -1,7 +1,8 @@
 initWorkspace
 
-selectMap = 1:4;
-selectResTestGrid = 1:6;
+selectMap = 3;
+selectResTestGrid = 4; %{1:'debug';2:'coarse';3:'medium';4:'fine';'finer';'finest'};
+
 
 Map_ResTestGrid = getAllComb(selectMap,selectResTestGrid);
 Map_ResTestGrid=sortrows(Map_ResTestGrid,2); % sort with increasing computing time;
@@ -10,5 +11,5 @@ for ii=1:size(Map_ResTestGrid,1)
     Map_ii = Map_ResTestGrid(ii,1);
     ResTestGrid_ii = Map_ResTestGrid(ii,2);
 %     LPT_Benchmark_calc(Map_ii,ResTestGrid_ii)
-	LPT_Benchmark_plot(Map_ii,ResTestGrid_ii,0)
+	LPT_Benchmark_plot(Map_ii,ResTestGrid_ii,1)
 end
