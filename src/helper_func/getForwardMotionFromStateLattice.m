12fb59eabfa748f248a105042c6d0fa85a32a6fc
Kevin Denis <kevin.denis@student.kuleuven.be>
2017-08-11 20:15:39 +0200

    Added removal of Turn On Spot

--- src/helper_func/getForwardMotionFromStateLattice.m
+++ src/helper_func/getForwardMotionFromStateLattice.m
@@ -1,5 +1,6 @@
 function [LSL_Fwd] = getForwardMotionFromStateLattice(LSL)
 %[LSL_Fwd] = getForwardMotionFromStateLattice(LSL)
 LSL_Fwd=LSL(all([[LSL.x1]>=0;[LSL.x0]>=0],1));
+LSL_Fwd=removeToSFromLSL(LSL_Fwd);
 end
 
